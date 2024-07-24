64e601daff513932a16d0ddbd2714b16ea63b5b4
Deepakdekidugi <dk376907@gmail.com>
2024-07-24 16:29:17 +0530

    Added few more lines of code for postman

--- Create_a_Server/server.js
+++ Create_a_Server/server.js
@@ -1,10 +1,27 @@
-// import http from 'http';
+let http=require("http")
+let fs=require("fs");
+const { log } = require("console");
 
-let http = required("http");
+console.log("starting server");
 
-http
-    .createServer((req, res) => {
-        res.write("Hello World!");
-        res.end();
-    })
-    .listen(8080);
+http.createServer((req,res)=>{
+fs.readFile('index.html',(err,data)=>{
+    if(!err){
+        res.writeHead(200,{
+            "Content-Type":"text/html",
+        })
+        res.write(data)
+        return res.end()
+    }
+    res.write("error while reading file"+err)
+    return res.end()
+    
+})
+//     console.log({header:req.headers});
+//     console.log({req});
+//   res.write("hello world")
+//   res.end();
+})
+.listen(8080,()=>{
+    console.log("welcome")
+})
