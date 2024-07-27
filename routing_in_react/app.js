baf3d3734fda985b55316a7bc4fcbdf70e680aa9
Deepakdekidugi <dk376907@gmail.com>
2024-07-27 14:38:51 +0530

    fixed error

--- routing_in_react/app.js
+++ routing_in_react/app.js
@@ -2,30 +2,50 @@ const express = require("express");
 
 const PORT = 8080;
 const app = express();
-app.use(express.json());
 
 const blogs = [];
 
-app.get("/health", (req,res) => {
-    res.send("ok");
+app.use(express.json());
+
+// !TODO: mount routes
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
+// Create
 app.post("/blog", (req, res) => {
-    const {body} =req;
-    const {author, content} = body;
-    
-    if(author && content){
-        blogs.push({author, content});
-        res.send("OK");
-        return;
-    }
-    res.status(400).send("!OK");
+  //   console.log(req.body);
+  const { body } = req;
+
+  const { author, content } = body;
+
+  if (author && content) {
+    blogs.push({ author, content });
+    res.send("OK");
+    return;
+  }
+  res.status(400).send("!OK");
 });
 
-app.get("./blog", (req, res) => {
-    res.json(blogs).send();
+// Read all blogs
+app.get("/blog", (req, res) => {
+  res.status(200).json(blogs).send();
+});
+
+// Read individual blog using ID
+app.get("/blog/:blogId", (req, res) => {
+  let { blogId } = req.params;
+  console.log(blogId);
+
+  if (blogId > 0 && blogId <= blogs.length) {
+    blogId -= 1;
+    const blogToReturn = blogs[blogId];
+    return res.status(200).json(blogToReturn).send();
+  }
+  //   res.json(blogs).send();
+  res.status(404).send();
 });
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-})
+  console.log(`Server running on port: ${PORT}`);
+});
